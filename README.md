582d7d73b4bca45219cad30fbc60e7a02926585a
godlykira <MINTHIT.23@ichat.sp.edu.sg>
2023-11-15 02:43:17 +0800

    Changes - remove artifacts - added weapons

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # genshin_but_express
+
 This is a BED-CA1 assignment repo.
+
+<iframe width="560" height="315" src='https://dbdiagram.io/e/6549b7317d8bbd6465a0f08f/6553a1457d8bbd64652d2b67'> </iframe>
---
+++ scripts/add_character_to_db.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const readDirectoryRecursiveWithFilter = (baseDir, prefix, predicate) => {
+  const traverse = (folder) => {
+    const items = fs.readdirSync(`${prefix}/${folder}`);
+    items.forEach((file) => {
+      const path = `${folder}/${file}`;
+      if (fs.lstatSync(`${prefix}/${path}`).isDirectory()) {
+        traverse(path);
+        return;
+      }
+
+      predicate(path);
+    });
+  };
+  traverse(baseDir);
+};
